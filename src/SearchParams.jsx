116827c6001a59085cf6c6532378f6b71f27a8b8
nebojsa <nebojsa.brnjasevic@gmail.com>
2020-11-30 14:17:27 +0100

    async calls to Patfinder API, update Pet component

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,41 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
- 
+
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
     // const [location "<== current state!!!", setLocation "<== updater function!"] = useState("Seattle, WA");
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+    const [pets, setPets] = useState([]);
+
+    async function requestPets() {
+        const { animals } = await pet.animals({
+            location,
+            breed,
+            type: animal,
+        });
+
+        setPets(animals || []);
+    }
 
     useEffect(() => {
         // pet.breeds("dog").then(console.log, console.error);
         setBreeds([]);
         setBreed("");
 
-        pet.breeds(animal).then(({ breeds }) => {
-            const breedStrings = breeds.map(({name}) => name);
+        pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+            const breedStrings = apiBreeds.map(({ name }) => name);
             setBreeds(breedStrings);
-        }, console.error)
+        }, console.error);
     }, [animal, setBreed, setBreeds]); // <= leave blank to run only once
 
     return (
         <div className="search-params">
-            <form action="">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    requestPets();
+                }}
+            >
                 <label htmlFor="location">
                     Location
-                    <input id="location" 
-                            value={location}
-                            placeholder="Location" 
-                            onChange={event => setLocation(event.target.value)}/>
+                    <input
+                        id="location"
+                        value={location}
+                        placeholder="Location"
+                        onChange={(event) => setLocation(event.target.value)}
+                    />
                 </label>
                 <AnimalDropdown />
                 <BreedDropdown />
                 <button>Submit</button>
             </form>
+            <Results pets={pets} />
         </div>
-    )
-}
+    );
+};
 
 export default SearchParams;
